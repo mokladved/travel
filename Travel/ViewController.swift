f459f4da2c41cc1645de7f5adeb58b171cb91783
moklad <dev.moklad@gmail.com>
2025-07-12 15:51:35 +0900

    feat: 최초 프로젝트 생성 및 .gitignore 추가

---
+++ Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
