e452cd69ac4564a21b2c3e40ceecb30808b5d16c
moklad <dev.moklad@gmail.com>
2025-07-12 17:40:26 +0900

    feat(TapBar): 탭바 커스텀을 위한 TabBarController 추가

---
+++ Travel/CustomTabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTabBarController.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/12/25.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
