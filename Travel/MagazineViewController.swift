46178305b938b2b1e48010de8abb630c31d3d6f3
moklad <dev.moklad@gmail.com>
2025-07-12 17:03:39 +0900

    feat: MagazineViewController를 Root View로 설정

--- Travel/MagazineViewController.swift
+++ Travel/MagazineViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MagazineViewController.swift
 //  Travel
 //
 //  Created by Youngjun Kim on 7/12/25.
@@ -7,19 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MagazineViewController: UIViewController {
 
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
     
-    
-    
-
 }
-
