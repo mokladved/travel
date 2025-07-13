96f57460bf39184f7e441d6856464a2a50e1ffdb
moklad <dev.moklad@gmail.com>
2025-07-13 15:57:49 +0900

    feat(Magazine): 커스텀셀 클래스 생성 및 outlet 정의

---
+++ Travel/MagazineTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MagazineTableViewCell.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/13/25.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+    @IBOutlet var cellView: UIView!
+    
+    @IBOutlet var photoImageView: UIImageView!
+    
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var subtitleLabel: UILabel!
+    
+    @IBOutlet var dateLabel: UILabel!
+    
+    
+}
