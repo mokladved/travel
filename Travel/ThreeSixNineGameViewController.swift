09592ea42da258d62d8bb6f2233ba7ab834deb5f
moklad <dev.moklad@gmail.com>
2025-07-13 21:29:18 +0900

    feat(Game): ThreeSixNineGameViewController 생성 및 클래스 연결

---
+++ Travel/ThreeSixNineGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ThreeSixNineGameViewController.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/13/25.
+//
+
+import UIKit
+
+class ThreeSixNineGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
