01c2d082dc031f3f1ae0889732b7ca949aede639
moklad <dev.moklad@gmail.com>
2025-07-14 01:50:42 +0900

    feat(TabBar): TabBar UI 설정

---
+++ Travel/CustomTabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTabBarController.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/14/25.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    private func setUI() {
+        let appearance = UITabBarAppearance()
+        
+        appearance.stackedLayoutAppearance.normal.iconColor = .black
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.black]
+        
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+    }
+}
