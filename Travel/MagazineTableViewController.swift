4c4faa5139b3bed43e7479b45b357bfaaa654216
moklad <dev.moklad@gmail.com>
2025-07-13 16:04:33 +0900

    feat(Magazine): 테이블뷰 구현을 위한 기본 메서드 정의

--- Travel/MagazineTableViewController.swift
+++ Travel/MagazineTableViewController.swift
@@ -7,10 +7,28 @@
 
 import UIKit
 
-class MagazineTableViewController: UITableViewController {
+private class MagazineTableViewController: UITableViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "travelCell", for: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
 }
+
