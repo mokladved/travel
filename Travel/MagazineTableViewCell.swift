c1607476ff59d77996b2cb309e8337b34c106ed6
moklad <dev.moklad@gmail.com>
2025-07-13 20:48:15 +0900

    feat(Magazine): 셀 공통 style 적용

--- Travel/MagazineTableViewCell.swift
+++ Travel/MagazineTableViewCell.swift
@@ -7,16 +7,47 @@
 
 import UIKit
 
-class MagazineTableViewCell: UITableViewCell {
+final class MagazineTableViewCell: UITableViewCell {
     @IBOutlet var cellView: UIView!
-    
     @IBOutlet var photoImageView: UIImageView!
-    
     @IBOutlet var titleLabel: UILabel!
-    
     @IBOutlet var subtitleLabel: UILabel!
-    
     @IBOutlet var dateLabel: UILabel!
     
+    override func awakeFromNib() {
+        setUI()
+    }
     
+    private func setUI() {
+        setCellUI()
+        setPhotoImageViewUI()
+        setTitleLabellUI()
+        setSubtitleLabellUI()
+        setDateLabelUI()
+    }
+    
+    private func setCellUI() {
+        selectionStyle = .none
+    }
+    
+    private func setTitleLabellUI() {
+        titleLabel.numberOfLines = 0
+        titleLabel.font = .systemFont(ofSize: 18, weight: .heavy)
+    }
+    
+    private func setSubtitleLabellUI() {
+        subtitleLabel.font = .systemFont(ofSize: 13)
+        subtitleLabel.textColor =  .lightGray
+    }
+    
+    private func setPhotoImageViewUI() {
+        photoImageView.layer.cornerRadius = 10
+        photoImageView.clipsToBounds = true
+
+    }
+    
+    private func setDateLabelUI() {
+        dateLabel.textColor = .lightGray
+        dateLabel.font = .systemFont(ofSize: 13)
+    }
 }
