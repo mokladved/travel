0f1cf291fe13fff68ec76a574e5fa24e49886479
moklad <dev.moklad@gmail.com>
2025-07-13 14:36:44 +0900

    refactor(Magazine): 테이블뷰 활용을 위해 VC를 TableViewControll로 교체

---
+++ Travel/MagazineTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineTableViewController.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/13/25.
+//
+
+import UIKit
+
+class MagazineTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
