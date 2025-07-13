453e8a77bae4ad4f606e7255929daec9a4ac531f
moklad <dev.moklad@gmail.com>
2025-07-13 17:06:40 +0900

    feat(Magazine): photoImageView에 이미지 설정

--- Travel/MagazineTableViewController.swift
+++ Travel/MagazineTableViewController.swift
@@ -24,11 +24,15 @@ final class MagazineTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "travelCell", for: indexPath) as! MagazineTableViewCell
         
+        let rawUrl = magazineInfo.magazine[indexPath.row].photoImage
+        let imageUrl = URL(string: rawUrl)
+        cell.photoImageView.kf.setImage(with: imageUrl)
+        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return 350
     }
     
 }
