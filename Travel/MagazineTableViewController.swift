401c2bb6c80b38e16725585b5801ce2a4894b483
moklad <dev.moklad@gmail.com>
2025-07-13 16:59:48 +0900

    fix(Magazine): 잘못된 접근 제어자 수정

--- Travel/MagazineTableViewController.swift
+++ Travel/MagazineTableViewController.swift
@@ -6,28 +6,29 @@
 //
 
 import UIKit
+import Kingfisher
 
-private class MagazineTableViewController: UITableViewController {
-
+final class MagazineTableViewController: UITableViewController {
+    
+    let magazineInfo = MagazineInfo()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return magazineInfo.magazine.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "travelCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "travelCell", for: indexPath) as! MagazineTableViewCell
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
+        return 200
     }
     
 }
