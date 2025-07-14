52bc30b03abe4041951df95ad8e0d04ddfa32123
moklad <dev.moklad@gmail.com>
2025-07-14 15:14:25 +0900

    feat(Travel): TableViewController, TableViewCell 추가 및 기본 설정
    - TableViewCell 및 xib 파일 생성
    - identifier 설정 및 연결
    - TableViewController 필요 메서드 추가

---
+++ Travel/TravelTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TravelTableViewCell.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/14/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
---
+++ Travel/TravelTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TravelTableViewController.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/14/25.
+//
+
+import UIKit
+
+class TravelTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let xib = UINib(nibName: "TravelTableViewCell", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "TravelTableViewCell")
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewCell", for: indexPath) as! TravelTableViewCell
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+}
