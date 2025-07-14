117fa51a04b01fed52b4385626d157324acdc039
moklad <dev.moklad@gmail.com>
2025-07-14 18:04:45 +0900

    feat(Travel): 셀 기본 UI 설정

--- Travel/TravelTableViewCell.swift
+++ Travel/TravelTableViewCell.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class TravelTableViewCell: UITableViewCell {
-
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var gradeLabel: UILabel!
+    @IBOutlet var saveCountLabel: UILabel!
+    
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUI()
     }
-
     
+    func setUI() {
+        setTitleLabelUI()
+        setDescriptionLabelUI()
+    }
+    
+    func setTitleLabelUI() {
+        titleLabel.font = .systemFont(ofSize: 15, weight: .bold)
+    }
+    
+    func setDescriptionLabelUI() {
+        descriptionLabel.font = .systemFont(ofSize: 15)
+        descriptionLabel.numberOfLines = 0
+    }
 }
--- Travel/TravelTableViewController.swift
+++ Travel/TravelTableViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TravelTableViewController: UITableViewController {
 
+    let travelInfo = TravelInfo().travel
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let xib = UINib(nibName: "TravelTableViewCell", bundle: nil)
         tableView.register(xib, forCellReuseIdentifier: "TravelTableViewCell")
+        tableView.rowHeight = UITableView.automaticDimension
         
     }
     
@@ -22,6 +27,28 @@ class TravelTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewCell", for: indexPath) as! TravelTableViewCell
+        
+        let data = travelInfo[indexPath.row]
+        
+        if let title = data.title,
+           let desc = data.description,
+           let grade = data.grade,
+           let saveCount = data.save,
+           let imageUrl = data.travelImage,
+           let ad = data.ad,
+           let like = data.like {
+            cell.titleLabel.text = title
+            cell.descriptionLabel.text = desc
+            cell.gradeLabel.text = "\(grade)"
+            cell.saveCountLabel.text = "\(saveCount)"
+            
+            let url = URL(string: imageUrl)
+            cell.photoImageView.kf.setImage(with: url)
+            
+            let heartImage = like ? "star.fill" : "star"
+            cell.likeButton.setImage(UIImage(systemName: heartImage), for: .normal)
+            cell.likeButton.setTitle("", for: .normal)
+        }
         
         return cell
     }
