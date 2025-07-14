05bafc58920c323abb0a8d280d9fada239d33fda
moklad <dev.moklad@gmail.com>
2025-07-14 14:18:08 +0900

    feat(Travel): 더미데이터 추가 및 매핑된 구조체 생성

---
+++ Travel/Travel.swift
@@ -0,0 +1,29 @@
+//
+//  Travel.swift
+//  Travel
+//
+//  Created by Youngjun Kim on 7/14/25.
+//
+
+import Foundation
+
+struct Travel {
+    let title: String?
+    let description: String?
+    let travelImage: String?
+    let grade: Double?
+    let save: Int?
+    let like: Bool?
+    let ad: Bool?
+    
+    init(title: String?, description: String?, travel_image: String?, grade: Double?, save: Int?, like: Bool?, ad: Bool?) {
+        self.title = title
+        self.description = description
+        self.travelImage = travel_image
+        self.grade = grade
+        self.save = save
+        self.like = like
+        self.ad = ad
+    }
+    
+}
