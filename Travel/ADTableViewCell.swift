e88360794689f6f816c4c74f993e69ab07fe4c7b
moklad <dev.moklad@gmail.com>
2025-07-15 22:12:55 +0900

    feat(Cell): 광고를 위한 xib UI 변경

--- Travel/ADTableViewCell.swift
+++ Travel/ADTableViewCell.swift
@@ -8,17 +8,35 @@
 import UIKit
 
 class ADTableViewCell: UITableViewCell {
-
+    
+    let adColors: [UIColor] = [.adPink, .adGreen]
+    
+    @IBOutlet var adButton: UIButton!
+    
+    @IBOutlet var descLabel: UILabel!
+    
     @IBOutlet var adLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
     }
     
     func setCellUI(from data: Travel) {
-        adLabel.textAlignment = .center
-        adLabel.numberOfLines = 2
-        adLabel.font = .systemFont(ofSize: 15, weight: .bold)
-        adLabel.text = data.title
+        let backgroundColor = adColors[adColors.count % 2]
+        
+        adButton.setTitle("", for: .normal)
+        adButton.backgroundColor = backgroundColor
+        adButton.layer.cornerRadius = 5
+        adButton.clipsToBounds = true
+        
+        descLabel.textAlignment = .center
+        descLabel.numberOfLines = 0
+        descLabel.font = .systemFont(ofSize: 15, weight: .bold)
+        descLabel.text = data.title
+        
+        adLabel.text = "AD"
+        adLabel.font = .systemFont(ofSize: 12, weight: .semibold)
+        adLabel.layer.borderWidth = 1
+        adLabel.clipsToBounds = true
+        adLabel.layer.cornerRadius = 3
     }
-    
 }
