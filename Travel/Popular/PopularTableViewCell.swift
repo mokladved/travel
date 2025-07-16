93fb97f574b1e203e244b56c58fc22ae6ce9b5c6
moklad <dev.moklad@gmail.com>
2025-07-16 23:00:33 +0900

    feat(Cell): 셀 스타일 설정

--- Travel/Popular/PopularTableViewCell.swift
+++ Travel/Popular/PopularTableViewCell.swift
@@ -10,18 +10,39 @@ import UIKit
 final class PopularTableViewCell: UITableViewCell {
     
     @IBOutlet var imageWrappedView: UIView!
-    
+    @IBOutlet var cityLabel: UILabel!
+    @IBOutlet var explainLabel: UILabel!
+    @IBOutlet var cityimage: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        setImageView()
+        setCellUI()
     }
     
-    func setImageView() {
+    func setCellUI() {
+        setImageWrappedViewUI()
+        setCityLabelUI()
+        setExplainLabelUI()
+    }
+    
+    func setImageWrappedViewUI() {
         imageWrappedView.layer.cornerRadius = 20
         imageWrappedView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
         imageWrappedView.clipsToBounds = true
+        imageWrappedView.backgroundColor = .red
     }
-
+    
+    func setCityLabelUI() {
+        cityLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        cityLabel.textColor = .black
+        cityLabel.text = "가나다라마바사아"
+        cityLabel.textAlignment = .right
+    }
+    
+    func setExplainLabelUI() {
+        explainLabel.backgroundColor = .black.withAlphaComponent(0.3)
+        explainLabel.font = .systemFont(ofSize: 15)
+        explainLabel.textColor = .white
+    }
     
 }
